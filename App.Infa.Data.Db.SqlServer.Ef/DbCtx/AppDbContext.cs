e8795e93e97e493e0852983c3f7253c318cef966
Zoheir Allafzadeh <116369321+zoheirallafzadeh@users.noreply.github.com>
2024-01-28 21:49:21 +0330

    startEF

--- App.Infa.Data.Db.SqlServer.Ef/DbCtx/AppDbContext.cs
+++ App.Infa.Data.Db.SqlServer.Ef/DbCtx/AppDbContext.cs
@@ -1,27 +1,20 @@
 ﻿
 using App.Domain.Core.Tickets.Entities;
 using App.Domain.Core.Units.Entities;
+using App.Domain.Core.Users.Entitis;
 using Microsoft.EntityFrameworkCore;
 
 namespace App.Infa.Data.Db.SqlServer.Ef.DbCtx
 {
-    public class AppDbContext : IdentityDbContext<User,Role,int>
+    public class AppDbContext : DbContext
 
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Messege> Messeges { get; set; }
         public DbSet<Unit> Units { get; set; }
         public DbSet<UnitPart> Parts { get; set; }
-        public DbSet<Messege> Messeges { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
 
-public class User :IdentityUser<int>
-{
-    public string FristName { get; set; }
-    public string LastName { get; set; }
-}
-public class Role : IdentityRole<int>
-{
-    public string RoleFa { get; set; }
-}
